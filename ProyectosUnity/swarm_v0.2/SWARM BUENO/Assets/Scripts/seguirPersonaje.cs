40e1de715110e5a1c89dbbe67feebcc277dc8ef3
Eduard <eduardnavarro21@gmail.com>
2017-11-21 16:39:54 +0100

    ACTUALIZADO
    NUEVA VERSIÓN LIGERA

---
+++ ProyectosUnity/swarm_v0.2/SWARM BUENO/Assets/Scripts/seguirPersonaje.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seguirPersonaje : MonoBehaviour {
+
+    public Transform personaje;
+    public float altura = 0f;
+
+    void Update()
+    {
+        transform.position = new Vector3(personaje.position.x, personaje.position.y+altura, transform.position.z);
+
+    }
+}
